793f73a86769eada315b287f6734f84eafd9d353
Jianwei Xie <xiejw0217@gmail.com>
2019-03-27 08:23:33 -0700

    Print out the wday.

--- tools/cron/main.cpp
+++ tools/cron/main.cpp
@@ -10,10 +10,10 @@ void print_time(time_t current_time) {
 
   localtime_r(&current_time, result.get());
 
-  std::cout << "hello\n";
   std::cout << "Current epoch is " << current_time << std::endl;
   std::cout << "Year: " << result->tm_year + 1900 << std::endl;
   std::cout << "Month: " << result->tm_mon + 1 << std::endl;
+  std::cout << "Day of week: " << result->tm_wday << std::endl;
   std::cout << "Day: " << result->tm_mday << std::endl;
   std::cout << "Hour: " << result->tm_hour << std::endl;
   std::cout << "Minute: " << result->tm_min << std::endl;
