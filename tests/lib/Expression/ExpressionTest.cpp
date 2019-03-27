81010346915a8a9c5e42b37d3f311b2d8e0c53b8
Jianwei Xie <xiejw0217@gmail.com>
2019-03-27 12:55:25 -0700

    Add more tests.

--- tests/lib/Expression/ExpressionTest.cpp
+++ tests/lib/Expression/ExpressionTest.cpp
@@ -19,10 +19,40 @@ class ExpressionTest : public ::testing::Test {
 TEST_F(ExpressionTest, CheckMinute) {
   // Wednesday, March 27, 2019 11:31:58 AM GMT-07:00 DST
   const time_t start_time = 1553711518;
-  auto expression = Expression(/* minute =*/Field::MakeSingleValue(40));
+  auto expression = Expression(/*minute = */ Field::MakeSingleValue(40));
 
   // Wednesday, March 27, 2019 11:40:00 AM GMT-07:00
   ASSERT_EQ(1553712000, NextTime(expression, start_time));
+}
+
+TEST_F(ExpressionTest, CheckHour) {
+  // Wednesday, March 27, 2019 11:31:58 AM GMT-07:00 DST
+  const time_t start_time = 1553711518;
+  auto expression = Expression(/*minute = */ Field::MakeAny(),
+                               /*hour = */ Field::MakeSingleValue(13));
+
+  // Wednesday, March 27, 2019 1:00:00 PM GMT-07:00
+  ASSERT_EQ(1553716800, NextTime(expression, start_time));
+}
+
+TEST_F(ExpressionTest, CheckHourInNextDay) {
+  // Wednesday, March 27, 2019 11:31:58 AM GMT-07:00 DST
+  const time_t start_time = 1553711518;
+  auto expression = Expression(/*minute = */ Field::MakeAny(),
+                               /*hour = */ Field::MakeSingleValue(9));
+
+  // Thursday, March 28, 2019 9:00:00 AM GMT-07:00
+  ASSERT_EQ(1553788800, NextTime(expression, start_time));
+}
+
+TEST_F(ExpressionTest, EndToEnd) {
+  // Wednesday, March 27, 2019 11:31:58 AM GMT-07:00 DST
+  const time_t start_time = 1553711518;
+  auto expression = Expression(/*minute = */ Field::MakeSingleValue(13),
+                               /*hour = */ Field::MakeSingleValue(9));
+
+  // Thursday, March 28, 2019 9:13:00 AM GMT-07:00
+  ASSERT_EQ(1553789580, NextTime(expression, start_time));
 }
 
 }  // namespace
