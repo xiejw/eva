5623ef29c09788aaaeeccc2a23cfcedb28de67a3
Jianwei Xie <xiejw0217@gmail.com>
2019-04-21 20:44:59 -0700

    Add path expand.

--- tools/eva-config/main.cpp
+++ tools/eva-config/main.cpp
@@ -1,3 +1,35 @@
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 
-int main(int argc, char** argv) { return 0; }
+#include <wordexp.h>
+
+namespace {
+
+void FatalError(const char* fmt, ...) {
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  va_end(args);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+std::string ExpandPath(const std::string& path) {
+  wordexp_t exp_result;
+  if (int error = wordexp(path.c_str(), &exp_result, 0)) {
+    FatalError("Failed to expand the path: %s", path.c_str());
+  }
+  std::string result{exp_result.we_wordv[0]};
+  wordfree(&exp_result);
+  return result;
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  std::string src{"~/hello"};
+  std::cout << src << " -> " << ExpandPath(src);
+  return 0;
+}
