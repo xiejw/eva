ed524d3fc143d56841bdd5ffe840fbc21f4c1539
Jianwei Xie <xiejw0217@gmail.com>
2019-04-22 12:47:04 -0700

    Added Formulas into eva-config.

--- tools/eva-config/main.cpp
+++ tools/eva-config/main.cpp
@@ -2,8 +2,12 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <memory>
+#include <vector>
 
 #include <wordexp.h>
+
+constexpr char const* kEvaPath = "~/Workspace/eva";
 
 namespace {
 
@@ -16,6 +20,7 @@ void FatalError(const char* fmt, ...) {
   exit(1);
 }
 
+/// Expands the `path` according to the shell-style.
 std::string ExpandPath(const std::string& path) {
   wordexp_t exp_result;
   if (int error = wordexp(path.c_str(), &exp_result, 0)) {
@@ -28,8 +33,51 @@ std::string ExpandPath(const std::string& path) {
 
 }  // namespace
 
+namespace {
+
+struct Formula {
+  std::string name;
+  std::string cxxflags;
+  std::string ldflags;
+
+  Formula(const char* a_name, const char* a_cxxflags, const char* a_ldflags)
+      : name{a_name}, cxxflags{a_cxxflags}, ldflags{a_ldflags} {}
+};
+
+struct OutputInfo {
+  bool print_cxxflags;
+  bool print_ldflags;
+  Formula* formula;
+};
+
+class Outputs {
+ public:
+  Outputs() : formulars_{}, infos_{} {
+    // Initialze the information table. Right now, we just hard code them.
+    formulars_.push_back(std::make_unique<Formula>(
+        /*name=*/"gflags",
+        /*cxxflags=*/"-Idependencies/gflags/buildlib/include/",
+        /*ldflags=*/"dependencies/gflags/buildlib/lib/libgflags_nothreads.a"));
+
+    formulars_.push_back(std::make_unique<Formula>(
+        /*name=*/"googletest",
+        /*cxxflags=*/"-Idependencies/googletest/googletest/include/",
+        /*ldflags=*/
+        "dependencies/googletest/buildlib/lib/libgtest.a -lpthread"));
+  }
+
+ public:
+  void Print() {}
+
+ private:
+  std::vector<std::unique_ptr<Formula>> formulars_;
+  std::vector<std::unique_ptr<OutputInfo>> infos_;
+};
+
+}  // namespace
+
 int main(int argc, char** argv) {
-  std::string src{"~/Workspace/eva"};
+  std::string src{kEvaPath};
   std::cout << src << " -> " << ExpandPath(src);
   return 0;
 }
