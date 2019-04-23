01caa9731268928522bd49abae72229838ec351e
Jianwei Xie <xiejw0217@gmail.com>
2019-04-23 13:28:29 -0700

    Add ldflags printing.

--- tools/eva-config/main.cpp
+++ tools/eva-config/main.cpp
@@ -9,6 +9,8 @@
 #include <wordexp.h>
 
 constexpr const char* kEvaPath = "~/Workspace/eva";
+constexpr const char* kDependencyFolder = "dependencies";
+constexpr const char* kEvaPathEnvVarName = "EVA_PATH";
 
 namespace {
 
@@ -43,7 +45,9 @@ struct Formula {
   std::string ldflags;
 
   Formula(const char* a_name, const char* a_cxxflags, const char* a_ldflags)
-      : name{a_name}, cxxflags{a_cxxflags}, ldflags{a_ldflags} {}
+      : name{a_name},
+        cxxflags{ExpandPath(a_cxxflags)},
+        ldflags{ExpandPath(a_ldflags)} {}
 };
 
 struct OutputInfo {
@@ -58,14 +62,14 @@ class Outputs {
     // Initialze the information table. Right now, we just hard code them.
     formulars_.push_back(std::make_unique<Formula>(
         /*name=*/"gflags",
-        /*cxxflags=*/"-Idependencies/gflags/buildlib/include/",
-        /*ldflags=*/"dependencies/gflags/buildlib/lib/libgflags_nothreads.a"));
+        /*cxxflags=*/"-I${EVA_PATH}/gflags/buildlib/include/",
+        /*ldflags=*/"${EVA_PATH}/gflags/buildlib/lib/libgflags_nothreads.a"));
 
     formulars_.push_back(std::make_unique<Formula>(
         /*name=*/"googletest",
-        /*cxxflags=*/"-Idependencies/googletest/googletest/include/",
+        /*cxxflags=*/"-I${EVA_PATH}/googletest/googletest/include/",
         /*ldflags=*/
-        "dependencies/googletest/buildlib/lib/libgtest.a -lpthread"));
+        "${EVA_PATH}/googletest/buildlib/lib/libgtest.a -lpthread"));
   }
 
  public:
@@ -83,8 +87,24 @@ class Outputs {
     FatalError("Failed to find library with name %s", name);
   };
 
+  void RegisterLDFlags(const char* name) {
+    for (auto& formula : formulars_) {
+      // Tries to find a match.
+      if (strcmp(formula->name, name) == 0) {
+        auto info = std::make_unique<OutputInfo>();
+        info->print_ldflags = true;
+        info->formula = formula.get();
+        infos_.push_back(std::move(info));
+        return;
+      }
+    }
+    FatalError("Failed to find library with name %s", name);
+  };
+
   void Print() {
     bool first_one = true;
+
+    // First pass: Print cxxflags
     for (auto& info : infos_) {
       if (!info->print_cxxflags) continue;
       if (first_one)
@@ -92,6 +112,16 @@ class Outputs {
       else
         std::cout << " ";
       std::cout << info->formula->cxxflags;
+    }
+
+    // Second pass: Print ldflags
+    for (auto& info : infos_) {
+      if (!info->print_ldflags) continue;
+      if (first_one)
+        first_one = false;
+      else
+        std::cout << " ";
+      std::cout << info->formula->ldflags;
     }
   }
 
@@ -121,6 +151,11 @@ class Parser {
         continue;
       }
 
+      if (strcmp(argv_[current_index], "--ldflags") == 0) {
+        ParseLDFLags(outputs.get(), &current_index);
+        continue;
+      }
+
       FatalError("Failed to parse the flag %s", argv_[current_index]);
     }
 
@@ -131,14 +166,24 @@ class Parser {
   void ParseCXXFLags(Formula::Outputs* outputs, int* current_index) {
     while (true) {
       ++*current_index;
-
       // End of the tokens. So treat as empty arguments for --cxxflags.
       if (*current_index == argc_) return;
-
       // End of expected tokens.
       if (argv_[*current_index][0] == '-') return;
 
       outputs->RegisterCXXFlags(argv_[*current_index]);
+    }
+  };
+
+  void ParseLDFLags(Formula::Outputs* outputs, int* current_index) {
+    while (true) {
+      ++*current_index;
+      // End of the tokens. So treat as empty arguments for --cxxflags.
+      if (*current_index == argc_) return;
+      // End of expected tokens.
+      if (argv_[*current_index][0] == '-') return;
+
+      outputs->RegisterLDFlags(argv_[*current_index]);
     }
   };
 
@@ -148,9 +193,18 @@ class Parser {
 };
 }  // namespace Parser
 
+namespace {
+
+void PrepareEnvironmentVariableForEvaPath() {
+  // EVA_PATH=${kEvaPath}/${kDependencyFolder}
+  std::string full_eval_path = ExpandPath(kEvaPath) + "/" + kDependencyFolder;
+  setenv(kEvaPathEnvVarName, full_eval_path.c_str(), 1);
+}
+
+}  // namespace
+
 int main(int argc, char** argv) {
-  std::string src{kEvaPath};
-  std::cout << src << " -> " << ExpandPath(src) << "\n";
+  PrepareEnvironmentVariableForEvaPath();
   Parser::Parser parser{argc, argv};
   parser.Parse()->Print();
   return 0;
