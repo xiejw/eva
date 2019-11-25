91840aa53510293fca85aa35f1eb1bb90c016994
Jianwei Xie <xiejw0217@gmail.com>
2019-11-25 15:37:08 -0800

    Started Refactoring the code.

---
+++ lib/FileSystem/Walk.h
@@ -0,0 +1,29 @@
+#ifndef EVA_LIB_FILESYSTEM_WALK
+#define EVA_LIB_FILESYSTEM_WALK
+
+#include <string>
+
+namespace eva {
+namespace fs {
+
+struct WalkStat {
+  // A valid path in the OS. Should be accesiable via current directory of
+  // processs.
+  std::string full_path;
+
+  // Relative path to root.
+  std::string d_path;
+
+  // The file (dir) name.
+  std::string f_path;
+
+  bool is_folder;
+  long long size;
+};
+
+void WalkTree(const char* root_path);
+
+}  // namespace fs
+}  // namespace eva
+
+#endif
