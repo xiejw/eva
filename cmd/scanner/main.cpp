c847bf0f957c8a6dade958a629a5f0c05168a1c8
Jianwei Xie <xiejw0217@gmail.com>
2019-11-25 14:15:03 -0800

    Improve the result

--- cmd/scanner/main.cpp
+++ cmd/scanner/main.cpp
@@ -1,28 +1,57 @@
-#include <dirent.h>
 #include <cstring>
 #include <iostream>
 #include <string>
+
+#include <dirent.h>
+#include <sys/stat.h>
 
 #include "lib/Support/Error.h"
 
 namespace {
 
 struct Stat {
+  std::string full_path;
+  std::string d_path;
   std::string f_path;
   bool is_folder;
+  long long size;
 };
 
-void PrintFileStat(const Stat& stat) {
-  std::cout << stat.f_path;
-  if (stat.is_folder) std::cout << "@D";
-  std::cout << "\n";
+std::string PathJoin(const char* dir, const char* f_name) {
+  if (dir == nullptr || strcmp(dir, "") == 0) return f_name;
+
+  std::string r{dir};
+  if (f_name != nullptr && strcmp(f_name, "") != 0) {
+    r += "/";
+    r += f_name;
+  }
+  return r;
 }
 
-void ListFiles(const char* dirpath) {
+void ListFiles(const char*, const char*);
+
+void PrintFileStat(const char* root_path, const Stat& stat) {
+  // Common fiter
+  if (stat.is_folder && stat.f_path.find(".", 0) == 0) return;
+
+  auto relative_path = PathJoin(stat.d_path.c_str(), stat.f_path.c_str());
+  if (stat.is_folder)
+    std::cout << "@\t" << relative_path.c_str();
+  else
+    std::cout << stat.size << "\t" << relative_path.c_str();
+  std::cout << "\n";
+
+  if (stat.is_folder) {
+    ListFiles(root_path, relative_path.c_str());
+  }
+}
+
+void ListFiles(const char* root_path, const char* relative_d) {
+  auto d_path = PathJoin(root_path, relative_d);
   DIR* dirp;
-  dirp = opendir(dirpath);
+  dirp = opendir(d_path.c_str());
   if (dirp == nullptr) {
-    eva::FatalError("opendir failed on '%s'", dirpath);
+    eva::FatalError("opendir failed on '%s'", d_path.c_str());
     return;
   }
 
@@ -35,9 +64,22 @@ void ListFiles(const char* dirpath) {
     if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
       continue;  // Skip . and ..
 
-    // d_type is not POSIX.
-    PrintFileStat(
-        Stat{/* f_path =*/dp->d_name, /* is_folder =*/dp->d_type == DT_DIR});
+    {
+      struct stat sb;
+      auto f_path = dp->d_name;
+      auto full_path = PathJoin(d_path.c_str(), f_path);
+      if (stat(full_path.c_str(), &sb) == -1) {
+        printf("error stat %s: %d\n", full_path.c_str(), errno);
+        continue;
+      }
+
+      Stat st{/*full_path=*/full_path,
+              /*d_path*/ (relative_d == nullptr) ? "" : relative_d,
+              /*f_path=*/f_path,
+              /* is_folder =*/dp->d_type == DT_DIR,
+              /*size=*/sb.st_size};
+      PrintFileStat(root_path, st);
+    }
   }
 
   if (errno != 0) eva::FatalError("readdir");
@@ -48,6 +90,6 @@ void ListFiles(const char* dirpath) {
 
 int main(int argc, char** argv) {
   std::cout << "Hello\n";
-  ListFiles(".");
+  ListFiles(".", nullptr);
   return 0;
 }
