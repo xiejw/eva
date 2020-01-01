fdd1eadf66a200bda7cc1d3690f601c84d9f1737
Jianwei Xie <xiejw0217@gmail.com>
2019-12-31 20:44:09 -0800

    Added flags into scanner.

--- cmd/scanner/main.cpp
+++ cmd/scanner/main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 
+#include <gflags/gflags.h>
+
 #include "lib/FileSystem/FileSystem.h"
 
+DEFINE_string(dir, ".", "The directory to scan.");
+DEFINE_int32(max_num_files, -1, "The nax number of files to scan.");
+
 constexpr bool kFetchChecksum = true;
-constexpr int kMaxNumFiles = 5;
 
 int main(int argc, char** argv) {
-  eva::fs::FileTree tree{".", {kFetchChecksum, kMaxNumFiles}};
+  // Sets up gflags.
+  gflags::SetUsageMessage("scanner");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  // Sets up the max_num_files.
+  std::optional<int> max_num_files{};
+  if (FLAGS_max_num_files > 0) max_num_files = FLAGS_max_num_files;
+
+  eva::fs::FileTree tree{FLAGS_dir.c_str(), {kFetchChecksum, max_num_files}};
   tree.Refresh();
 
   for (auto& handle : tree) {
