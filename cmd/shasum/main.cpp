551fc5e85384bbaaa95efde669c65854c6221368
Jianwei Xie <xiejw0217@gmail.com>
2020-01-01 17:10:10 -0800

    Added shasum.

--- cmd/shasum/main.cpp
+++ cmd/shasum/main.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
+#include <string>
 
-int main(int argc, char** argv) { std::cout << "Hello " << argc << "\n"; }
+#include "lib/Crypto/Sha256.h"
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cout << "Need one argument for file.\n";
+    return 1;
+  }
+
+  std::string digest{};
+  if (eva::crypto::SHA256::DigestForFile(argv[1], digest)) {
+    std::cout << "Error during shasum evaluation..\n";
+    return 3;
+  }
+  std::cout << digest << "\n";
+}
