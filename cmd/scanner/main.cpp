72e19ebd35d323608d21ad2ea9eef537372ef736
Jianwei Xie <xiejw0217@gmail.com>
2020-01-04 15:46:31 -0800

    Added database flag.

--- cmd/scanner/main.cpp
+++ cmd/scanner/main.cpp
@@ -7,8 +7,15 @@
 
 DEFINE_string(dir, ".", "The directory to scan.");
 DEFINE_int32(max_num_files, -1, "The nax number of files to scan.");
+DEFINE_string(
+    database, "",
+    "The pattern pointing the checksum database. "
+    "If unspecified, i.e., empty string ``, checksum results will be printed.");
 
 constexpr bool kFetchChecksum = true;
+
+// Line by line prints the `FileTree`.
+void printTree(const eva::fs::FileTree&);
 
 int main(int argc, char** argv) {
   // Sets up gflags.
@@ -22,11 +29,22 @@ int main(int argc, char** argv) {
   eva::fs::FileTree tree{FLAGS_dir.c_str(), {kFetchChecksum, max_num_files}};
   tree.Refresh();
 
+  if (FLAGS_database.empty()) {
+    // If database is not specified, just prints results.
+    printTree(tree);
+    return 0;
+  }
+
+  std::cout << "Unsupported for db.\n";
+
+  return 1;
+}
+
+void printTree(const eva::fs::FileTree& tree) {
   for (auto& handle : tree) {
     if (kFetchChecksum)
       std::cout << handle->checksum.value() << " " << handle->path << "\n";
     else
       std::cout << handle->size << "\t" << handle->path << "\n";
   }
-  return 0;
 }
