bf69b2f16ffcf7b325d0975e941066045d86c039
Jianwei Xie <xiejw0217@gmail.com>
2020-01-05 14:01:20 -0800

    Refactor the code.

--- lib/FileSystem/FileReader.cpp
+++ lib/FileSystem/FileReader.cpp
@@ -11,34 +11,45 @@ namespace eva::fs {
 
 namespace {
 
-constexpr unsigned int kBufferSize = 12; // 4096 * 16;
-constexpr unsigned int kMaxLineLen = 500;
+// Byte size to read file block.
+constexpr unsigned int kBufferSize = 4096 * 16;
 
 }  // namespace
 
+void FileReader::loadNextBuffer() {
+  pos_ = 0;
+  allocated_ = read(fd_, (void *)(buffer_.get()), kBufferSize);
+
+  if (allocated_ == -1)
+    eva::FatalError("Faied to read file for %s: %s", file_name_.c_str(),
+                    strerror(errno));
+}
+
 FileReader::~FileReader() {
-  if (fd_ != -1 && close(fd_) == -1) {
+  if (fd_ != -1 && close(fd_) == -1)
     eva::FatalError("Faied to close file for %s: %s", file_name_.c_str(),
                     strerror(errno));
-  }
+
+  fd_ = -1;
 }
 
 std::optional<std::string> FileReader::nextline() {
   if (end_of_file_) return {};
 
-  // Initialize all states.
+  // Initialize all states, if applicable.
   if (!buffer_) {
     fd_ = open(file_name_.c_str(), O_RDONLY);
-    if (fd_ == -1) {
+    if (fd_ == -1)
       eva::FatalError("Faied to open file for %s: %s", file_name_.c_str(),
                       strerror(errno));
-    }
+
     buffer_.reset(new unsigned char[kBufferSize]);
     pos_ = 0;
     allocated_ = 0;
   }
 
-  unsigned char line[kMaxLineLen];
+  // Allocates the buffer for current line.
+  unsigned char line[max_line_len_];
   int current_len = 0;
 
   // The loop ends in any of the following condition.
@@ -50,7 +61,6 @@ std::optional<std::string> FileReader::nextline() {
 
     // Checks whether it is end-of-file.
     if (allocated_ == 0) {
-      line[current_len] = '\0';
       end_of_file_ = true;
       return std::string((const char *)line, current_len);
     }
@@ -60,13 +70,12 @@ std::optional<std::string> FileReader::nextline() {
     for (auto i = pos_; i < allocated_; ++i) {
       if (buffer_[i] == '\n') {
         int length = i - pos_;
-        if (current_len + length + 1 >= kMaxLineLen) {
+        if (current_len + length >= max_line_len_) {
           eva::FatalError("Line length is too long.");
         }
         memcpy((void *)(line + current_len), (void *)(buffer_.get() + pos_),
                length);
         current_len += length;
-        line[current_len] = '\0';
         pos_ = i + 1;
         return std::string((const char *)line, current_len);
       }
@@ -74,7 +83,7 @@ std::optional<std::string> FileReader::nextline() {
 
     // We reach the end of the buffer.
     int length = allocated_ - pos_;
-    if (current_len + length + 1 >= kMaxLineLen) {
+    if (current_len + length >= max_line_len_) {
       eva::FatalError("Line length is too long.");
     }
     memcpy((void *)(line + current_len), (void *)(buffer_.get() + pos_),
@@ -82,16 +91,6 @@ std::optional<std::string> FileReader::nextline() {
     current_len += length;
     pos_ = allocated_;
     // next iteration.
-  }
-}
-
-void FileReader::loadNextBuffer() {
-  pos_ = 0;
-  allocated_ = read(fd_, (void *)(buffer_.get()), kBufferSize);
-
-  if (allocated_ == -1) {
-    eva::FatalError("Faied to read file for %s: %s", file_name_.c_str(),
-                    strerror(errno));
   }
 }
 
--- lib/FileSystem/FileReader.h
+++ lib/FileSystem/FileReader.h
@@ -1,6 +1,7 @@
 #ifndef EVA_LIB_FILESYSTEM_FILEREADER_
 #define EVA_LIB_FILESYSTEM_FILEREADER_
 
+#include <cassert>
 #include <optional>
 #include <string>
 
@@ -9,18 +10,27 @@ namespace eva::fs {
 // A file reader which reads line by line.
 class FileReader {
  public:
-  FileReader(std::string file_name) : file_name_{std::move(file_name)} {}
+  // `max_line_len` specifies the max line length. If excceed, triggers a fatal
+  // error.
+  FileReader(std::string file_name, unsigned int max_line_len = 500)
+      : file_name_{std::move(file_name)}, max_line_len_{max_line_len} {
+    assert(max_line_len_ > 0);
+  }
+
+  // Cleans the resource.
   ~FileReader();
 
  public:
-  // Returns emtpy optional if end of file.
+  // Returns empty optional if end of file.
   std::optional<std::string> nextline();
 
  private:
+  // Loads next block from file.
   void loadNextBuffer();
 
  private:
   std::string file_name_;
+  const unsigned int max_line_len_;
 
   int fd_ = -1;
   std::unique_ptr<unsigned char[]> buffer_ = {};
