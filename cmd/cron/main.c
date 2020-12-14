9c92ad26be79654c1fa383ab201eec607bd15b28
Jianwei Xie <xiejw0217@gmail.com>
2020-12-13 21:25:29 -0800

    added getopt for cron.

--- cmd/cron/main.c
+++ cmd/cron/main.c
@@ -1,31 +1,86 @@
-#include <time.h>
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "cron/expr.h"
 
-// DEFINE_int32(minute, -1, "The minute field. `-1` means `*`.");
-// DEFINE_int32(hour, -1, "The hour field. `-1` means `*`.");
+typedef int error_t;
 
-// std::unique_ptr<eva::cron::Field> ReadFromFlag(
-//     const gflags::int32& flag_value) {
-//   if (flag_value == -1) return eva::cron::Field::MakeAny();
-//
-//   return eva::cron::Field::MakeSingleValue(flag_value);
-// }
+// -----------------------------------------------------------------------------
+// helper methods prototypes.
+// -----------------------------------------------------------------------------
 
-// Prints the number of seconds from `now` to the next time matching
-// eva::cron::Expression.
+static error_t parseOptions(cron_expr_t* expr, int argc, char** argv);
+static error_t parsePosInt(char* str, int* v);
+
+// -----------------------------------------------------------------------------
+// implementation.
+// -----------------------------------------------------------------------------
+
+// print the number of seconds from `now` to the next time cron expression.
 int main(int argc, char** argv) {
-  // gflags::ParseCommandLineFlags(&argc, &argv, true);
+  cron_expr_t expr;
+  cronExprInit(&expr);
+
+  if (parseOptions(&expr, argc, argv)) exit(1);
 
   time_t current_time = time(NULL);
   time_t next_time;
 
-  cron_expr_t expr;
-  cronExprInit(&expr);
-  // Error happened.
   if (cronExprNext(&expr, current_time, &next_time)) exit(1);
-
   printf("%ld", (long)(next_time - current_time));
+  return 0;
+}
+
+// -----------------------------------------------------------------------------
+// helper methods. implementation.
+// -----------------------------------------------------------------------------
+
+static struct option longopts[] = {{"hour", required_argument, NULL, 'h'},
+                                   {"minute", required_argument, NULL, 'm'},
+                                   {NULL, 0, NULL, 0}};
+
+error_t parseOptions(cron_expr_t* expr, int argc, char** argv) {
+  int ch;
+  int v;
+  while ((ch = getopt_long(argc, argv, "h:m:", longopts, NULL)) != -1) {
+    switch (ch) {
+      case 'm':
+        if (parsePosInt(optarg, &v)) return -1;
+        cronFieldSetSingleValue(&expr->minute, v);
+        break;
+      case 'h':
+        if (parsePosInt(optarg, &v)) return -1;
+        cronFieldSetSingleValue(&expr->hour, v);
+        break;
+      default:
+        printf("error on parsing options.\n");
+        exit(-1);
+    }
+  }
+  if (argv[optind] != NULL) {
+    fprintf(stderr, "remaining options cannot be parsed: %s\n", argv[optind]);
+    return -1;
+  }
+  return 0;
+}
+
+error_t parsePosInt(char* str, int* v) {
+  if (*str == '\0') {
+    fprintf(stderr, "int argument cannot be empty string\n");
+    return -1;
+  }
+
+  char* end_p;
+  *v = (int)strtol(str, &end_p, /*base=*/10);
+  if (*end_p != '\0') {
+    fprintf(stderr, "int argument cannot be parsed: %s\n", str);
+    return -1;
+  }
+  if (*v < 0) {
+    fprintf(stderr, "require positive int argument: %d\n", *v);
+    return -1;
+  }
   return 0;
 }
