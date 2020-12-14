ca8f93e40aab55caef2434993816c6aea0b8027c
Jianwei Xie <xiejw0217@gmail.com>
2020-12-13 16:50:09 -0800

    added more typedef.

--- src/cron/expr.c
+++ src/cron/expr.c
@@ -1,27 +1,33 @@
 #include "cron/expr.h"
 
-enum Component { minute, hour, day, dayOfWeek, month, _final_component_marker };
+typedef struct tm tm_t;
+
+typedef enum {
+  cron_minute,
+  cron_hour,
+  cron_day,
+  cron_day_of_week,
+  cron_month,
+  cron_final_component_marker
+} cron_comp_t;
 
 // -----------------------------------------------------------------------------
 // helper methods prototypes.
 // -----------------------------------------------------------------------------
 
-static void rewind(struct tm* candidate, enum Component up_to);
-static int  validate(const struct tm* const candidate,
-                     const struct tm* const start_time);
-static int  value(struct tm* time_tm, enum Component component);
-static void increase(struct tm* time_tm, enum Component component);
-static int  searchNextMatching(const struct tm* const start_time,
-                               cron_field_t*          field,
-                               const enum Component   component,
-                               struct tm* candidate, int* changed);
+static void rewind(tm_t* candidate, cron_comp_t up_to);
+static int  validate(tm_t* candidate, tm_t* start_time);
+static int  value(tm_t* time_tm, cron_comp_t comp);
+static void increase(tm_t* time_tm, cron_comp_t comp);
+static int  searchNextMatching(tm_t* start_time, cron_field_t* field,
+                               cron_comp_t comp, tm_t* candidate, int* changed);
 
 // -----------------------------------------------------------------------------
 // implementation.
 // -----------------------------------------------------------------------------
 
 int cronExprMatch(cron_expr_t* expr, time_t time) {
-  struct tm time_tm;
+  tm_t time_tm;
   localtime_r(&time, &time_tm);
 
   if (!cronFieldMatch(&expr->minute, time_tm.tm_min)) return 0;
@@ -45,9 +51,9 @@ int cronExprMatch(cron_expr_t* expr, time_t time) {
 //           (hour and minute in this case) and jump to `mainLoop`.
 // Step 5-6: Search month and year. Same rule as step 3 and 4.
 int cronExprNext(cron_expr_t* expr, time_t start_time, time_t* next_time) {
-  struct tm candidate;
+  tm_t candidate;
   localtime_r(&start_time, &candidate);
-  struct tm start_point = candidate;  // make a copy.
+  tm_t start_point = candidate;  // make a copy.
 
   // Advance the minute once to start the search and reset the second field.
   candidate.tm_min += 1;
@@ -55,23 +61,23 @@ int cronExprNext(cron_expr_t* expr, time_t start_time, time_t* next_time) {
 
 mainLoop:
   while (1) {
-    for (enum Component component = minute; component < _final_component_marker;
-         component++) {
+    for (cron_comp_t comp = cron_minute; comp < cron_final_component_marker;
+         comp++) {
       cron_field_t* field;
-      switch (component) {
-        case minute:
+      switch (comp) {
+        case cron_minute:
           field = &expr->minute;
           break;
-        case hour:
+        case cron_hour:
           field = &expr->hour;
           break;
-        case day:
+        case cron_day:
           field = &expr->day;
           break;
-        case dayOfWeek:
+        case cron_day_of_week:
           field = &expr->day_of_week;
           break;
-        case month:
+        case cron_month:
           field = &expr->month;
           break;
         default:
@@ -82,7 +88,7 @@ mainLoop:
       int changed;
       if (searchNextMatching(/*start_time=*/&start_point,
                              /*field=*/field,
-                             /*component=*/component,
+                             /*comp=*/comp,
                              /*candidate=*/&candidate,
                              /*changed=*/&changed))
         return -1;
@@ -102,51 +108,50 @@ mainLoop:
 
 // Rewinds all components in the candidate up to `up_to`. For example, if
 // `up_to` is `day`, then `hour`, `minute` are rewinded to `zero`.
-void rewind(struct tm* candidate, enum Component up_to) {
+void rewind(tm_t* candidate, cron_comp_t up_to) {
   switch (up_to) {
-    case month:
+    case cron_month:
       candidate->tm_mday = 1;
       // fallthrough
-    case dayOfWeek:
+    case cron_day_of_week:
       // fallthrough
-    case day:
+    case cron_day:
       candidate->tm_hour = 0;
       // fallthrough
-    case hour:
+    case cron_hour:
       candidate->tm_min = 0;
       break;
-    case minute:
+    case cron_minute:
       // no-op
       break;
     default:
       assert(0);
-      // FatalError("Unexpected component: %d", up_to);
+      // FatalError("Unexpected comp: %d", up_to);
   }
 }
 
 // Validates the candidate. We only search up to 5 years.
-int validate(const struct tm* const candidate,
-             const struct tm* const start_time) {
+int validate(tm_t* candidate, tm_t* start_time) {
   return candidate->tm_year - start_time->tm_year > 5;
 }
 
 // Retrieves the component from the time.
-int value(struct tm* time_tm, enum Component component) {
-  switch (component) {
-    case minute:
+int value(tm_t* time_tm, cron_comp_t comp) {
+  switch (comp) {
+    case cron_minute:
       return time_tm->tm_min;
-    case hour:
+    case cron_hour:
       return time_tm->tm_hour;
-    case day:
+    case cron_day:
       return time_tm->tm_mday;
-    case dayOfWeek:
+    case cron_day_of_week:
       return time_tm->tm_wday;
-    case month:
-      // Month starts with 0 in `tm`.
+    case cron_month:
+      // Month starts with 0 in `tm_t`.
       return time_tm->tm_mon + 1;
     default:
       assert(0);
-      // FatalError("Unexpected component: %d", component);
+      // FatalError("Unexpected comp: %d", comp);
   }
 }
 
@@ -155,51 +160,50 @@ int value(struct tm* time_tm, enum Component component) {
 //
 // For example: if the minute reaches 60, minute will be reset as 0 and hour
 // will be increaesed.
-void increase(struct tm* time_tm, enum Component component) {
-  switch (component) {
-    case minute:
+void increase(tm_t* time_tm, cron_comp_t comp) {
+  switch (comp) {
+    case cron_minute:
       time_tm->tm_min += 1;
       break;
-    case hour:
+    case cron_hour:
       time_tm->tm_hour += 1;
       break;
-    case day:
+    case cron_day:
       time_tm->tm_mday += 1;
       break;
-    case dayOfWeek:
+    case cron_day_of_week:
       time_tm->tm_mday += 1;
       break;
-    case month:
+    case cron_month:
       time_tm->tm_mon += 1;
       break;
     default:
       assert(0);
-      // FatalError("Unexpected component: %d", component);
+      // FatalError("Unexpected comp: %d", comp);
   }
   mktime(time_tm);
 }
 
-// Focusing only on the component inside the `candidate`, searching the next
+// Focusing only on the comp inside the `candidate`, searching the next
 // value, by keeping increasing it, until matching the `field` of the
 // expression.
-int searchNextMatching(const struct tm* const start_time, cron_field_t* field,
-                       const enum Component component, struct tm* candidate,
-                       int* changed) {
-  int initial_value = value(candidate, component);
+int searchNextMatching(tm_t* start_time, cron_field_t* field, cron_comp_t comp,
+                       tm_t* candidate, int* changed) {
+  int initial_value = value(candidate, comp);
   if (cronFieldMatch(field, initial_value)) {
     *changed = 0;
     return 0;
   }
 
   *changed = 1;
-  rewind(candidate, component);
+  rewind(candidate, comp);
 
   while (1) {
-    increase(candidate, component);
+    increase(candidate, comp);
     if (validate(candidate, start_time)) {
       return -1;
     }
-    int new_value = value(candidate, component);
+    int new_value = value(candidate, comp);
 
     if (cronFieldMatch(field, new_value)) {
       return 0;
