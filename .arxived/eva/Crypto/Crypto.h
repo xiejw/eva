c95415229e6d50ce050e9ded2d06053ad2e5fcc8
Jianwei Xie <xiejw0217@gmail.com>
2021-05-24 17:24:35 -0700

    added stream test for sha256.

--- .arxived/eva/Crypto/Crypto.h
+++
@@ -1,1 +0,0 @@
-#include "eva/Crypto/Sha256.h"
--- src/crypto/sha256_test.c
+++ src/crypto/sha256_test.c
@@ -52,11 +52,31 @@ test_long_str()
         return NULL;
 }
 
+static char*
+test_stream()
+{
+        const char* msg = "grape";
+
+        struct sha256_t s;
+        sha256Reset(&s);
+        for (int i = 0; i < 5; i++) {
+                sha256Update(&s, (unsigned char*)msg + i, 1);
+        }
+        sds_t sds = sha256Digest(&s);
+
+        ASSERT_TRUE("digest", 0 == strcmp("0f78fcc486f5315418fbf095e71c0675ee07"
+                                          "d318e5ac4d150050cd8e57966496",
+                                          sds));
+        sdsFree(sds);
+        return NULL;
+}
+
 char*
 run_crypto_sha256_suite()
 {
         RUN_TEST(test_empty_str);
         RUN_TEST(test_str);
         RUN_TEST(test_long_str);
+        RUN_TEST(test_stream);
         return NULL;
 }
