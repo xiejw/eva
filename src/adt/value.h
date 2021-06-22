8ff12c616b606a4b68c590af686a3dbcc0763e88
xiejw.gmac2021 <xiejw0217+gmac2021@gmail.com>
2021-06-21 21:04:43 -0700

    added more types.

--- src/adt/value.h
+++ src/adt/value.h
@@ -14,9 +14,11 @@ enum value_kind_t {
 struct value_t {
         enum value_kind_t kind;
         union {
-                int64_t i64;
-                float   f32;
-                void   *data;
+                int64_t  i64;
+                uint64_t u64;
+                double   f64;
+                float    f32;
+                void    *data;
         };
 };
 
@@ -25,18 +27,31 @@ struct value_t *valueNewI64(int64_t i);
 struct value_t *valueNewF32(float f);
 void            valueFree(struct value_t *);
 
-// Used for dict.
-int   valueCmpI64(void *privdata, const void *, const void *);
-int   valueCmpF32(void *privdata, const void *, const void *);
-void *valueDupI64(void *privdata, const void *);
-void *valueDupF32(void *privdata, const void *);
-void  valueFreeI64(void *privdata, void *);
-void  valueFreeF32(void *privdata, void *);
-
 #define valueGetKind(v) (((struct value_t *)(v))->kind)
 #define valueGetI64(v)  (((struct value_t *)(v))->i64)
+#define valueGetU64(v)  (((struct value_t *)(v))->u64)
 #define valueGetF32(v)  (((struct value_t *)(v))->f32)
+#define valueGetF64(v)  (((struct value_t *)(v))->f64)
+
+// Used for dict.
+int valueCmpI64(void *privdata, const void *, const void *);
+int valueCmpU64(void *privdata, const void *, const void *);
+int valueCmpF32(void *privdata, const void *, const void *);
+int valueCmpF64(void *privdata, const void *, const void *);
+
+void *valueDupI64(void *privdata, const void *);
+void *valueDupU64(void *privdata, const void *);
+void *valueDupF32(void *privdata, const void *);
+void *valueDupF64(void *privdata, const void *);
+
+void valueFreeI64(void *privdata, void *);
+void valueFreeu64(void *privdata, void *);
+void valueFreeF32(void *privdata, void *);
+void valueFreeF64(void *privdata, void *);
 
 uint64_t valueHashFnI64(const void *key);
+uint64_t valueHashFnU64(const void *key);
+uint64_t valueHashFnF32(const void *key);
+uint64_t valueHashFnF64(const void *key);
 
 #endif
