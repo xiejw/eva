2b8d6a7fec1134e4e77048c39f47945af3ce95ad
xiejw.gmac2021 <xiejw0217+gmac2021@gmail.com>
2021-06-21 17:12:22 -0700

    added apis.

--- src/adt/value.h
+++ src/adt/value.h
@@ -1,6 +1,8 @@
 #ifndef VALUE_H_
 #define VALUE_H_
 
+#include <stdint.h>
+
 // Represents a value in the core system used by many data strucutures, e.g.,
 // dict.
 
@@ -8,5 +10,19 @@ enum value_kind_t {
         VKIND_INT,
         VKIND_DATA,
 };
+
+struct value_t {
+        enum value_kind_t kind;
+        union {
+                int64_t i64;
+                float   f32;
+                void   *data;
+        };
+};
+
+struc value_t *valueNewData(void *data);
+struc value_t *valueNewI64(int64_t i);
+struc value_t *valueNewF32(float f);
+void           valueFree(struct value_t *);
 
 #endif
